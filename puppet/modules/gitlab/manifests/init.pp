bde501e5d0614efe42f8f758e162ab853c168c49
lennycartier <l.cartier@free.fr>
2014-11-18 23:14:31 +0100

    check if gitlab is already installed

--- puppet/modules/gitlab/manifests/init.pp
+++ puppet/modules/gitlab/manifests/init.pp
@@ -1,11 +1,8 @@
 class gitlab {
 
-	$once_lock = "/var/lock/puppet-once"
-
         exec { "install-gitlab":
-                command => "wget https://downloads-packages.s3.amazonaws.com/ubuntu-14.04/gitlab_7.4.3-omnibus.5.1.0.ci-1_amd64.deb && dpkg -i gitlab_7.4.3-omnibus.5.1.0.ci-1_amd64.deb",
-		creates => once_lock,
-                timeout => 0,
+                command => "dpkg -s gitlab 2>/dev/null >/dev/null || wget https://downloads-packages.s3.amazonaws.com/ubuntu-14.04/gitlab_7.4.3-omnibus.5.1.0.ci-1_amd64.deb && dpkg -i gitlab_7.4.3-omnibus.5.1.0.ci-1_amd64.deb",
+		timeout => 0,
         }
 
         file { 'gitlab.rb':
