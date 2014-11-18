8cbdcbaca13ecf4a804f0492ca556f4f5e6c0076
lennycartier <l.cartier@free.fr>
2014-11-18 23:12:54 +0100

    source /etc/gitlab.rb (puppet template)

--- puppet/modules/gitlab/manifests/init.pp
+++ puppet/modules/gitlab/manifests/init.pp
@@ -1,7 +1,17 @@
 class gitlab {
 
-	exec { "install-gitlab":
-        	command => "wget https://downloads-packages.s3.amazonaws.com/ubuntu-14.04/gitlab_7.4.3-omnibus.5.1.0.ci-1_amd64.deb && dpkg -i gitlab_7.4.3-omnibus.5.1.0.ci-1_amd64.deb",
-        	timeout => 0,
-	}
+	$once_lock = "/var/lock/puppet-once"
+
+        exec { "install-gitlab":
+                command => "wget https://downloads-packages.s3.amazonaws.com/ubuntu-14.04/gitlab_7.4.3-omnibus.5.1.0.ci-1_amd64.deb && dpkg -i gitlab_7.4.3-omnibus.5.1.0.ci-1_amd64.deb",
+		creates => once_lock,
+                timeout => 0,
+        }
+
+        file { 'gitlab.rb':
+        path    => '/etc/gitlab/gitlab.rb',
+        ensure  => file,
+        content => template("gitlab/gitlab.rb"),
+      }
+
 }
